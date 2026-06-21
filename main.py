3b9ff1b723e49df819e8917507275a374cc5444e
VenkataDivya1485 <venkata.nallamothu@gisma-student.com>
2026-06-21 16:43:16 +0200

    Implement restaurant system

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+from restaurant import Restaurant
 
+restaurant = Restaurant()
+restaurant.load_menu()
+
+while True:
+    print("\n----- RESTAURANT SYSTEM -----")
+    print("1. Show Menu")
+    print("2. Take Order")
+    print("3. Generate Bill")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        restaurant.show_menu()
+
+    elif choice == "2":
+        restaurant.take_order()
+
+    elif choice == "3":
+        restaurant.generate_customer_bill()
+
+    elif choice == "4":
+        print("Thank you! Visit again.")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
